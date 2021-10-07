83f3df2ed22166278d1ae27be704fe3f89efb60e
padmaattikeri <padma2.attikeri@gmail.com>
2021-10-07 23:09:20 +0530

    moved directory and shop data to its own reducers and created respective redux flow for directory and shop

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,55 +1,21 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import MenuItem from '../menu-item/menu-item.component';
+import {createStructuredSelector} from 'reselect';
 
+import { selectDirectorySection } from '../../redux/directory/directory.selectors';
 import './directory.styles.scss';
 
-class Directory extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5
-        }
-      ]
-    };
-  }
-
-  render() {
-    return (
+const Directory =({sections}) => ( 
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-        ))}
+        {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
       </div>
     );
-  }
-}
 
-export default Directory;
+const mapstateToProps = createStructuredSelector({
+  sections : selectDirectorySection,
+});
+
+export default connect(mapstateToProps)(Directory);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,25 @@
 import {combineReducers} from 'redux';
+import { persistReducer } from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
 
 import UserReducer from './user/user.reducer';
 import CartReducer from './cart/cart.reducer';
+import DirectoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
-export default combineReducers({
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist:['cart']
+}
+
+const rootReducer = combineReducers({
     user: UserReducer,
-    cart: CartReducer
+    cart: CartReducer,
+    directory: DirectoryReducer,
+    shop :shopReducer
 })
+
+export default persistReducer(persistConfig,rootReducer);
